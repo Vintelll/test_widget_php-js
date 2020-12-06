3aecc7591f6d8148eedcc8c72b3a8fbcac692d40
Vintelll <Vintelll@yandex.ru>
2020-12-06 15:12:44 +0300

    add basic database connector

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+
+$link = mysqli_connect('localhost', 'root', 'password', 'db_name');
+
+if(mysqli_connect_errno()){
+    echo 'Ошибка подключения к базе данных ('.mysqli_connect_errno().'): '. mysqli_connect_error();
+    exit();
+}
