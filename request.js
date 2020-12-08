f7728b5349adeaac9cfda41cc132d1353c412762
Konstantin Zhavoronkov <kzhavoronkov@gmail.com>
2020-12-08 23:32:00 +0300

    tests with DB

--- request.js
+++ request.js
@@ -1,16 +1,38 @@
 $(function() {
-    $('#Form').submit(function(e){
-        var $form = $(this);
+    $('.submit').click(function(e){
+        var $form = $('#Form');
         $.ajax({
             type:$form.attr('method'),
             url:$form.attr('action'),
             data:$form.serialize()
         }).done(function(data) {
             console.log('success');
-            console.log(JSON.parse(data).time);
+            console.log(JSON.parse(data));
         }).fail(function(){
             console.log('fail');
         });
         e.preventDefault();
+        });
+    $('button.minus').click(function(minus){
+        var currentValue = parseInt($("input[name='col']").val());
+        if ((currentValue - 1) > 0){
+            $("input[name='col']").val(currentValue - 1);
+        } else {
+            alert('Минимальное количество к заказу 1!');
+            $("input[name='col']").val(1);
+        }
+        minus.preventDefault();
+
+        });
+    $('button.plus').click(function(plus){
+        var currentValue = parseInt($("input[name='col']").val());
+        if ((currentValue) > 0){
+            $("input[name='col']").val(currentValue + 1);
+        } else {
+            alert('Минимальное количество к заказу 1!');
+            $("input[name='col']").val(1);
+        }
+
+        plus.preventDefault();
         });
     });
--- test.php
+++ test.php
@@ -1,3 +1,23 @@
-<?php 
-echo json_encode(array( "name"=>"John","time"=>"2pm" ));
+<?php
+// print_r($_GET)
+if($_GET['cart'] ==='add'){
+    $link = mysqli_connect('localhost', 'root', 'root', 'product_widget');
+    if(mysqli_connect_errno()){
+        echo json_encode(array('ошибка' => 'Ошибка подключения к базе данных'));
+        exit();
+    }
+    $data = array(
+        'count'=>intval($_GET['col']),
+        'ip'=>$_SERVER['REMOTE_ADDR']
+    );
+    $columns = implode(", ",array_keys($data));
+    $values  = implode(", ", array_values($data));
+    $sql = "INSERT INTO `orders`($columns) VALUES ($values)";
+    mysqli_query($link, $sql);
+    echo json_encode($values);
+}
+// $ip = $_SERVER['REMOTE_ADDR'];
+// $intIp = ip2long($ip);
+// echo json_encode(long2ip($intIp));
+// echo json_encode(array( "name"=>"John","time"=>"2pm" ));
 ?>
