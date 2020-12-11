d7d725aba27516926389a3bd37989e371a6128a4
Konstantin Zhavoronkov <kzhavoronkov@gmail.com>
2020-12-11 23:50:12 +0300

    html and css work

--- test.php
+++ test.php
@@ -1,23 +1,24 @@
 <?php
-// print_r($_GET)
 if($_GET['cart'] ==='add'){
     $link = mysqli_connect('localhost', 'root', 'root', 'product_widget');
     if(mysqli_connect_errno()){
         echo json_encode(array('ошибка' => 'Ошибка подключения к базе данных'));
         exit();
     }
-    $data = array(
-        'count'=>intval($_GET['col']),
-        'ip'=>$_SERVER['REMOTE_ADDR']
-    );
-    $columns = implode(", ",array_keys($data));
-    $values  = implode(", ", array_values($data));
-    $sql = "INSERT INTO `orders`($columns) VALUES ($values)";
-    mysqli_query($link, $sql);
-    echo json_encode($values);
-}
-// $ip = $_SERVER['REMOTE_ADDR'];
-// $intIp = ip2long($ip);
-// echo json_encode(long2ip($intIp));
-// echo json_encode(array( "name"=>"John","time"=>"2pm" ));
+    $count = intval($_GET['col']);
+    $ip  = strval($_SERVER['REMOTE_ADDR']);
+    $sql = "INSERT INTO `orders`(count, ip) VALUES ($count, '$ip')";
+    if(mysqli_query($link, $sql)){
+        $response = array(
+            'statusCode'=> 1,
+            'textReply'=> 'Товар добавлен в корзину'
+        );
+    }else{
+        $response = array(
+            'statusCode'=> 0,
+            'textReply'=> 'Ошибка при записи в базу данных'
+        );
+    };
+    echo json_encode($response);
+    };
 ?>
