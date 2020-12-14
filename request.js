0a4bac159dd53db454551e9c52adf3cd2fbd6182
Vintelll <Vintelll@yandex.ru>
2020-12-14 17:46:40 +0300

    add function declarations  for reuse

--- request.js
+++ request.js
@@ -1,4 +1,24 @@
 $(function() {
+    function countAlert(){
+        alert('Минимальное количество к заказу 1!');
+        $("input[name='col']").val(1);
+    };
+    function throwMessage(text, delay){
+        $('.response').text(text);
+        $('.hidden').removeClass('hidden');
+        setTimeout(function(){
+            $('.message').addClass('hidden');
+            $('.arrow').addClass('hidden');
+        }, delay);
+    };
+    function disableSubmit(delay){
+        $(".submit").attr("disabled", true);
+        $(".submit").addClass("inative")
+        setTimeout(function(){
+            $(".submit").attr("disabled", false);
+            $(".submit").removeClass("inative")
+        }, delay);
+    };
     $('.submit').click(function(e){
         var $form = $('#Form');
         $.ajax({
@@ -8,12 +28,7 @@ $(function() {
         }).done(function(data) {
             console.log('Ajax request success');
             console.log(JSON.parse(data));
-            $('.response').text(JSON.parse(data).textReply);
-            $('.hidden').removeClass('hidden');
-            setTimeout(function(){
-                $('.message').addClass('hidden');
-                $('.arrow').addClass('hidden');
-            }, 3000);
+            throwMessage(JSON.parse(data).textReply, 3000)
         }).fail(function(){
             console.log('Ajax request failed');
         });
@@ -24,27 +39,26 @@ $(function() {
         if ((currentValue - 1) > 0){
             $("input[name='col']").val(currentValue - 1);
         } else {
-            alert('Минимальное количество к заказу 1!');
-            $("input[name='col']").val(1);
+            countAlert();
+            disableSubmit(3000);
         }
         minus.preventDefault();
 
         });
     $('button.plus').click(function(plus){
         var currentValue = parseInt($("input[name='col']").val());
-        if ((currentValue) > 0){
-            $("input[name='col']").val(currentValue + 1);
-        } else {
-            alert('Минимальное количество к заказу 1!');
-            $("input[name='col']").val(1);
-        }
-
+        $("input[name='col']").val(currentValue + 1);
         plus.preventDefault();
-        });
+    });
     $("input[name='col']").change(function() {
         if ((this.value) <= 0){
-            alert('Минимальное количество к заказу 1!');
-            $("input[name='col']").val(1);
+            countAlert();
+            disableSubmit(3000);
+        }else if (!Number.isInteger(this.value)) {
+            disableSubmit(3000)
+            this.value = Math.round(this.value);
+            var message = "Допустимо только целое число!";
+            throwMessage(message, 3000);
         }
     });
     });
