4ba5406fecd37e8bef5ad77670eef34c9f8c47f4
Konstantin Zhavoronkov <kzhavoronkov@gmail.com>
2020-12-20 20:52:45 +0300

    final changes

--- add_to_cart.php
+++ add_to_cart.php
@@ -3,38 +3,37 @@
 $server = 'localhost';
 $username = 'root';
 $password = 'root';
-$dbName = 'product_widget';
-if($_GET['cart'] ==='add'){
+$dbName = 'shop';
+if ($_GET['cart'] === 'add') {
     mysqli_report(MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ALL);
     try {
-    $link = mysqli_connect($server, $username, $password, $dbName);
-    }catch (Exception $e){
+        $link = mysqli_connect($server, $username, $password, $dbName);
+    } catch (Exception $e) {
         $response = array(
-        'statusCode' => '0',
-        'textReply' => 'Ошибка подключения к базе данных',
-        'nativeErrorText' => mysqli_connect_error());
+            'statusCode' => '0',
+            'textReply' => 'Ошибка подключения к базе данных',
+            'nativeErrorText' => mysqli_connect_error()
+        );
         echo json_encode($response);
         exit();
     };
-    if($link){
+    if ($link) {
         $count = intval($_GET['col']);
         $ip  = strval($_SERVER['REMOTE_ADDR']);
         $sql = "INSERT INTO `orders`(count, ip) VALUES ($count, '$ip')";
-        try{
-            mysqli_query($link, $sql){
+        try {
+            mysqli_query($link, $sql);
             $response = array(
-                'statusCode'=> 1,
-                'textReply'=> 'Товар добавлен в корзину')
-            };
-        }catch(Exception $e){
+                'statusCode' => 1,
+                'textReply' => 'Товар добавлен в корзину'
+            );
+        } catch (Exception $e) {
             $response = array(
-                'statusCode'=> 0,
-                'textReply'=> 'Ошибка при записи в базу данных',
+                'statusCode' => 0,
+                'textReply' => 'Ошибка при записи в базу данных',
                 'nativeErrorText' => $e->getMessage()
             );
         };
         echo json_encode($response);
-        };
+    };
 };
-?>
-
--- request.js
+++ request.js
@@ -1,81 +1,88 @@
-$(function() {
-// Объявление переиспользуемых функций
-// функция отображения сообщений
-    function throwMessage(text, delay){
+$(function () {
+    // Объявление переиспользуемых функций
+    // функция отображения сообщений
+    function throwMessage(text, delay) {
         $('.response').text(text);
         $('.hidden').removeClass('hidden');
-        setTimeout(function(){
+        setTimeout(function () {
             $('.message').addClass('hidden');
             $('.arrow').addClass('hidden');
         }, delay);
     };
-// функция контроля минимального колличества
-    function countAlert(){
+    // функция контроля минимального колличества
+    function countAlert() {
         throwMessage('Минимальное количество к заказу 1!', 3000);
         $("input[name='col']").val(1);
     };
-// функция временной блокировки отправки формы
-    function disableSubmit(delay){
+    // функция временной блокировки отправки формы
+    function disableSubmit(delay) {
         $(".submit").attr("disabled", true);
         $(".submit").addClass("inative")
-        setTimeout(function(){
+        setTimeout(function () {
             $(".submit").attr("disabled", false);
             $(".submit").removeClass("inative")
         }, delay);
     };
-// функция отправки Ajax запроса
-    function sendAjax(){
+    // функция отправки Ajax запроса
+    function sendAjax() {
         var $form = $('#Form');
         $.ajax({
-            type:$form.attr('method'),
-            url:$form.attr('action'),
-            data:$form.serialize(),
+            type: $form.attr('method'),
+            url: $form.attr('action'),
+            data: $form.serialize(),
             // вывод в консоль GET запроса
-            beforeSend: function (){
+            beforeSend: function () {
                 console.log(this.url);
             }
-        }).done(function(data) {
+        }).done(function (data) {
             console.log('Ajax request success');
             // вывод в консоль response из PHP
             console.log(JSON.parse(data));
             throwMessage(JSON.parse(data).textReply, 3000)
-        }).fail(function(){
+        }).fail(function () {
             console.log('Ajax request failed');
         });
     };
-// Действия на события
-// Нажатие кнопки "КУПИТЬ"
-    $('.submit').click(function(submit){
-        sendAjax();
-        disableSubmit(3000);
-        submit.preventDefault();
-        });
-// Нажатие кнопки минус
-    $('a.minus').click(function(minus){
+    // Действия на события
+    // Нажатие кнопки "КУПИТЬ"
+    $('.submit').click(function (submit) {
         var currentValue = parseInt($("input[name='col']").val());
-        if ((currentValue - 1) > 0){
+        if (currentValue > 0) {
+            sendAjax();
+            disableSubmit(3000);
+        }
+        else {
+            countAlert();
+            disableSubmit(3000);
+        }
+        submit.preventDefault();
+    });
+    // Нажатие кнопки минус
+    $('a.minus').click(function (minus) {
+        var currentValue = parseInt($("input[name='col']").val());
+        if ((currentValue - 1) > 0) {
             $("input[name='col']").val(currentValue - 1);
         } else {
             countAlert();
             disableSubmit(3000);
         };
         minus.preventDefault();
-        });
-// Нажатие кнопки плюс
-    $('a.plus').click(function(plus){
+    });
+    // Нажатие кнопки плюс
+    $('a.plus').click(function (plus) {
         var currentValue = parseInt($("input[name='col']").val());
         $("input[name='col']").val(currentValue + 1);
         plus.preventDefault();
     });
-// Проверка значения при ручном вводе на отрицательные и не целые числа(округление в меньшую сторону)
-    $("input[name='col']").change(function() {
+    // Проверка значения при ручном вводе на отрицательные и не целые числа(округление в меньшую сторону)
+    $("input[name='col']").change(function () {
         var currentValue = parseInt(this.value)
-        if (currentValue <= 0){
+        if (currentValue <= 0) {
             countAlert();
             disableSubmit(3000);
         }
-        else{
+        else {
             $("input[name='col']").val(currentValue);
         };
     });
-    });
+});
